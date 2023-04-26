d92ca32b49e6d5092df6b04451a479ec65a74e7f
pandeyKaviraj <kaviraj.pandey11@gmail.com>
2023-04-26 12:32:14 +0530

    100 days of SwiftUI checkpoint 2 solution

--- Swift-Coding_Review/main.swift
+++ Swift-Coding_Review/main.swift
@@ -5,7 +5,18 @@
 //  Created by Kaviraj Pandey on 24/04/23.
 //
 
+// Important swift concepts to help you crack coding interveiws.
+
+
+
+
 import Foundation
 
-print("Hello, World!")
+// 100 days of swiftui checkpoint 2
+
+let albums = ["Red", "Blue", "Green", "Purple", "Red"]
+print("Total number of albums are \(albums.count)")
+
+let removeDuplicateAlbums = Set(albums)
+print(removeDuplicateAlbums.count)
 
